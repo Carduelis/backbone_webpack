be94423f1532340744b184ee97065b88234dda50
Carduelis <pavepy@gmail.com>
2017-08-23 14:28:29 +0300

    decreasing size of bundle

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,88 +1,96 @@
-const path = require("path");
-const webpack = require("webpack");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const WebpackRTLPlugin = require("webpack-rtl-plugin");
-const SERVER_ADDRESS = "localhost";
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const WebpackRTLPlugin = require('webpack-rtl-plugin');
+const SERVER_ADDRESS = 'localhost';
 
 module.exports = {
-	devtool: "cheap-module-eval-source-map",
-	entry: "./src/index.js",
+	entry: './src/index.js',
 	output: {
-		path: path.join(__dirname, `build`),
-		filename: "bundle.js",
-		publicPath: "./"
+		path: path.join(__dirname, 'build'),
+		filename: 'bundle.js',
+		publicPath: './'
 	},
 	module: {
 		rules: [
 			{
 				test: /\.less$/,
 				use: ExtractTextPlugin.extract({
-					fallback: "style-loader",
+					fallback: 'style-loader',
 					use: [
 						{
-							loader: "css-loader",
+							loader: 'css-loader',
 							options: {
 								minimize: true
 								// importLoaders: 1
 							}
 						},
 						{
-							loader: "postcss-loader",
+							loader: 'postcss-loader',
 							options: {
 								config: {
-									path: path.join(__dirname, "postcss.config.js")
+									path: path.join(__dirname, 'postcss.config.js')
 								}
 							}
 						},
-						"less-loader"
+						'less-loader'
 					]
 				})
 			},
 			{
 				test: /\.(png|jpg|gif|svg)$/,
-				loader: "file-loader?name=images/[name]_[hash:3].[ext]"
+				loader: 'file-loader?name=images/[name]_[hash:3].[ext]'
 			},
 			{
 				test: /\.(eot|ttf|woff|woff2)$/,
-				loader: "file-loader?name=fonts/[name]_[hash:3].[ext]"
+				loader: 'file-loader?name=fonts/[name]_[hash:3].[ext]'
 			},
 			{
 				test: /\.(js|ejs)$/,
 				exclude: /node_modules/,
 				use: [
 					{
-						loader: "babel-loader",
+						loader: 'babel-loader',
 						options: {
-							presets: ["es2015"]
+							presets: ['es2015']
 						}
 					}
 				]
 			},
 			{
 				test: /\.json$/,
-				loader: "json-loader"
+				loader: 'json-loader'
 			},
 			{
 				test: /\.(tmpl)$/,
 				exclude: /node_modules/,
-				loader: "ejs-loader"
+				loader: 'ejs-loader'
 			}
 		]
 	},
 	plugins: [
+		new webpack.LoaderOptionsPlugin({
+			minimize: true,
+			debug: false
+		}),
 		new webpack.optimize.UglifyJsPlugin({
+			minify: true,
+			beautify: false,
+			mangle: {
+				screw_ie8: true,
+				keep_fnames: true
+			},
 			compress: {
-				warnings: false
-			}
+				screw_ie8: true
+			},
+			comments: false
 		}),
 		new webpack.DefinePlugin({
-			"process.env": {
-				// Useful to reduce the size of client-side libraries, e.g. react
-				NODE_ENV: JSON.stringify("production"),
-				PLATFORM_ENV: JSON.stringify("web")
+			'process.env': {
+				NODE_ENV: JSON.stringify('production')
 			}
 		}),
-		new ExtractTextPlugin("bundle.css"),
+		new ExtractTextPlugin('bundle.css'),
 		new WebpackRTLPlugin()
 	]
 };
